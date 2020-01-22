6ad8b8796724fa25d5aba83489191604afe246d7
larryjl <leelawrencej@gmail.com>
2020-01-22 12:20:31 -0700

    Updates

---
+++ precache-manifest.1d2994b1992526c349b16816f36682e1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015c58183b668baf0aa485e16bfe5cf8",
+    "url": "/jupiter/index.html"
+  },
+  {
+    "revision": "052cf912969330a4b766",
+    "url": "/jupiter/static/css/main.0472857c.chunk.css"
+  },
+  {
+    "revision": "ac6a35b5af085bbc38a3",
+    "url": "/jupiter/static/js/2.3720566d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jupiter/static/js/2.3720566d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "052cf912969330a4b766",
+    "url": "/jupiter/static/js/main.798a96d4.chunk.js"
+  },
+  {
+    "revision": "a4c3f0ff392f825abfc7",
+    "url": "/jupiter/static/js/runtime-main.f95c9c89.js"
+  },
+  {
+    "revision": "8c03b2bc655df6b9bf08bcca80ef554b",
+    "url": "/jupiter/static/media/arrow-curved-blue.8c03b2bc.svg"
+  },
+  {
+    "revision": "ba5217d3bfff92ca1fa5d7c55b528aa6",
+    "url": "/jupiter/static/media/kuba_arrow_button_set_1.ba5217d3.svg"
+  },
+  {
+    "revision": "ef649afe91a493a4646dc5377a026389",
+    "url": "/jupiter/static/media/kuba_arrow_button_set_2.ef649afe.svg"
+  },
+  {
+    "revision": "8dd738298304deedb22c9111e2b52420",
+    "url": "/jupiter/static/media/kuba_arrow_button_set_3.8dd73829.svg"
+  },
+  {
+    "revision": "2659d3c4931584617d1e9c55bfe30ac1",
+    "url": "/jupiter/static/media/kuba_arrow_button_set_4.2659d3c4.svg"
+  }
+]);
