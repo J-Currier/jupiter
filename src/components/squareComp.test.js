5a2bf861628bda409a00ddd480045899660c9d2f
larryjl <leelawrencej@gmail.com>
2020-02-11 16:08:22 -0700

    feat: squareComp integration test

--- src/components/squareComp.test.js
+++ src/components/squareComp.test.js
@@ -1,10 +1,39 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
 import {Square, determineCorners} from "./squareComp.js";
 
-// test('divClick', () => {
-//     expect(functions.divClickFunction(0)).toBe(0);
+describe('functions', () => {
+    test('determine corners function', () => {
+        expect(determineCorners(1000, 1000, 200, 1)).toEqual([[1000, 1000],[1200, 1000],[1200, 800],[1000, 800]]);
+    });  
+});
 
-// });
-
-test('determine corners function', () => {
-    expect(determineCorners(1000, 1000, 200, 1)).toEqual([[1000, 1000],[1200, 1000],[1200, 800],[1000, 800]]);
+describe('square load', () => {
+    let container = null;
+    beforeEach(() => {
+      // setup a DOM element as a render target
+      container = document.createElement("div");
+      document.body.appendChild(container);
+    });
+    afterEach(() => {
+      // cleanup on exiting
+      unmountComponentAtNode(container);
+      container.remove();
+      container = null;
+    });
+    test('load', () => {
+        let shapeInfo = {
+            "id": "square",
+            "position": [0,0,100,1],
+            "fillColour": 'rgba(255, 77, 0, 1)',
+            "borderColour": 'rgba(255, 255, 255, 1)',
+            "borderWidth": 10,
+            "shapeClassName": ""
+          }
+        render(
+          <Square
+            shapeInfo={shapeInfo}
+          />, container
+        );
+    });
 });
