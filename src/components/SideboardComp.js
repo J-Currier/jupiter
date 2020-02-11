f71dbad8ea9b69f2a677b746e5c688cd855c0be1
larryjl <leelawrencej@gmail.com>
2020-02-11 13:29:22 -0700

    fix: cleanup and comment out undo button code

--- src/components/SideboardComp.js
+++ src/components/SideboardComp.js
@@ -1,71 +1,73 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import IconUp from '../images/kuba_arrow_button_set_3.svg'
 import IconLeft from '../images/kuba_arrow_button_set_1.svg'
 import IconRight from '../images/kuba_arrow_button_set_2.svg'
 import IconDown from '../images/kuba_arrow_button_set_4.svg'
-import IconUndo from '../images/arrow-curved-blue.svg'
+// import IconUndo from '../images/arrow-curved-blue.svg'
 import './Sideboard.css'
 
 function Sideboard(props) {
 
-    const [step, setStep] = useState(0); // todo
+    // const [step, setStep] = useState(0);
 
-    function HandleClick(e) {
+    function handleClick(e) {
         buttonInfo[e.target.name].fx(e)
     };
     const buttonInfo = {
-        up: {icon: IconUp, fx: (e)=>{props.buttonFunction(e, 0, -100)}},
-        left: {icon: IconLeft, fx: (e)=>{props.buttonFunction(e, -100, 0)}},
-        right: {icon: IconRight, fx: (e)=>{props.buttonFunction(e, 100, 0)}},
-        down: {icon: IconDown, fx: (e)=>{props.buttonFunction(e, 0, 100)}},
-        undo: {icon: IconUndo, fx: setStep},
-        redo: {icon: IconUndo, fx: setStep}
+        up: { icon: IconUp, fx: (e) => { props.buttonFunction(e, 0, -100) } },
+        left: { icon: IconLeft, fx: (e) => { props.buttonFunction(e, -100, 0) } },
+        right: { icon: IconRight, fx: (e) => { props.buttonFunction(e, 100, 0) } },
+        down: { icon: IconDown, fx: (e) => { props.buttonFunction(e, 0, 100) } }
     };
 
     const buttons = [];
-    for(let index in buttonInfo) {
-        if (index==="up" || index==="down" || index==="left" || index==="right") {
-            buttons.push(
-                <img 
-                    onClick={HandleClick} 
-                    key={index} 
-                    name={index} 
-                    id={index}
-                    className="directional button" 
-                    src= {buttonInfo[index].icon}
-                ></img>
-            );
-        };
+    for (let index in buttonInfo) {
+        buttons.push(
+            <img
+                onClick={handleClick}
+                key={index}
+                name={index}
+                alt={index}
+                id={index}
+                className="directional button"
+                src={buttonInfo[index].icon}
+            ></img>
+        );
     };
 
-    // const timeButtons = [];
-    // for(let index in buttonInfo) {
-    //     if (index==="undo" || index==="redo") {
-    //         timeButtons.push(
-    //             <img 
-    //                 onClick={HandleClick} 
-    //                 key={index} 
-    //                 name={index}
-    //                 id={index} 
-    //                 className="time button" 
-    //                 src= {buttonInfo[index].icon}
-    //             ></img>
-    //         );
-    //     };
-    // };
+    // const undoButtons = [
+    //     <img 
+    //         onClick={() => setStep(initial => initial -1)} 
+    //         key="undo"
+    //         name="undo"
+    //         alt="undo"
+    //         id="undo"
+    //         className="undo button" 
+    //         src= {IconUndo}
+    //     ></img>,
+    //     <img 
+    //         onClick={() => setStep(initial => initial +1)} 
+    //         key="redo"
+    //         name="redo"
+    //         alt="redo"
+    //         id="redo"
+    //         className="undo button" 
+    //         src= {IconUndo}
+    //     ></img>
+    // ];
 
     return (
-        <div id="sideboard">   
-            <input 
-                type="number" 
-                min='0' 
-                max='18' 
-                onChange={props.factorHandle} 
-                value={props.moveFactor} 
+        <div id="sideboard">
+            <input
+                type="number"
+                min='0'
+                max='18'
+                onChange={props.factorHandle}
+                value={props.moveFactor}
                 id="factorBox"
             />
             {buttons}
-            {/* {timeButtons} */}
+            {/* {undoButtons} */}
         </div>
     )
 }
