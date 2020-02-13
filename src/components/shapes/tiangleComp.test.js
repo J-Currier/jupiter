bf3823a012c7bce4e47a5c667a61cb576829d4c7
larryjl <leelawrencej@gmail.com>
2020-02-12 22:29:31 -0700

    fix: 6 orientations for triangle corners

--- src/components/shapes/tiangleComp.test.js
+++ src/components/shapes/tiangleComp.test.js
@@ -1,10 +1,59 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
 import {Triangle, determineCorners} from "./triangleComp.js";
 
-// test('divClick', () => {
-//     expect(functions.divClickFunction(0)).toBe(0);
-
-// });
-
-test('determine corners function', () => {
-    expect(determineCorners(1000, 1000, 200, 1)).toEqual([[1000, 1000],[1200, 1000],[1200, 800],[1000, 800]]);
+describe('determine corners', () => {
+    test('orientation 1', () => {
+        expect(determineCorners(1000, 1000, 200, 300, 1))
+          .toEqual([[1000, 1000],[1200, 1000],[1000, 700]]);
+    });
+    test('orientation 2', () => {
+        expect(determineCorners(1000, 1000, 200, 300, 2))
+          .toEqual([[1000, 1000],[1200, 1000],[1000, 1300]]);
+    });
+    test('orientation 3', () => {
+        expect(determineCorners(1000, 1000, 200, 300, 3))
+          .toEqual([[1000, 1000],[800, 1000],[1000, 1300]]);
+    });
+    test('orientation 4', () => {
+        expect(determineCorners(1000, 1000, 200, 300, 4))
+          .toEqual([[1000, 1000],[800, 1000],[1000, 700]]);
+    });
+    test('orientation 5', () => {
+        expect(determineCorners(1000, 1000, 200, 300, 5))
+          .toEqual([[1000, 1000],[1300, 1000],[1000, 1200]]);
+    });
+    test('orientation 6', () => {
+        expect(determineCorners(1000, 1000, 200, 300, 6))
+          .toEqual([[1000, 1000],[700, 1000],[1000, 800]]);
+    });
+});
+describe('triangle load', () => {
+    let container = null;
+    beforeEach(() => {
+      // setup a DOM element as a render target
+      container = document.createElement("div");
+      document.body.appendChild(container);
+    });
+    afterEach(() => {
+      // cleanup on exiting
+      unmountComponentAtNode(container);
+      container.remove();
+      container = null;
+    });
+    test('load', () => {
+        let shapeInfo = {
+            "id": 1,
+            "position": [0,0,100,1],
+            "fillColour": 'rgba(255, 77, 0, 1)',
+            "borderColour": 'rgba(255, 255, 255, 1)',
+            "borderWidth": 10,
+            "shapeClassName": ""
+          }
+        render(
+          <Triangle
+            shapeInfo={shapeInfo}
+          />, container
+        );
+    });
 });
--- src/components/shapes/triangleComp.js
+++ src/components/shapes/triangleComp.js
@@ -2,23 +2,20 @@ import React, { useEffect } from 'react';
 import './shapeComp.css';
 
 function Triangle(props) {
-    console.log('triangle')
     // postion is array of anchorX, anchorY, length
-    let {id, position, fillColour, borderColour, borderWidth, shapeClassName} = props.shapeInfo //props.circleInfo -> props.shapeInfo
+    let { id, position, fillColour, borderColour, borderWidth, shapeClassName } = props.shapeInfo //props.circleInfo -> props.shapeInfo
     let [anchorX, anchorY, size, orientation] = position
-    let length1= size*2;
-    let length2= 400
+    let length1 = size * 2;
+    let length2 = 400
 
     useEffect(() => {
         var canvas = document.getElementById(id);
         var context = canvas.getContext("2d");
         canvas.width = 2000;
         canvas.height = 2000;
-        console.log(anchorX, anchorY, length1, length2, orientation, 'preca')
         let cornerArray = determineCorners(anchorX, anchorY, length1, length2, orientation);
-        console.log(cornerArray, 'cornerarray')
 
-        function drawTriangle( fillColour, borderColour, borderWidth) {
+        function drawTriangle(fillColour, borderColour, borderWidth) {
             context.clearRect(0, 0, canvas.width, canvas.height)
             context.beginPath();
             context.moveTo(cornerArray[0][0], cornerArray[0][1]);
@@ -27,38 +24,33 @@ function Triangle(props) {
             context.moveTo(cornerArray[0][0], cornerArray[0][1]);
             context.fill();
             context.fillStyle = fillColour;
-            
+
             context.lineWidth = borderWidth;
             context.strokeStyle = borderColour;
             context.stroke();
         }
-            
-        drawTriangle(fillColour, borderColour, borderWidth); 
+
+        drawTriangle(fillColour, borderColour, borderWidth);
 
     }, [anchorX, anchorY, size, orientation]);
-    
+
     return (
-      <div className={shapeClassName}>
-          <canvas id={id}></canvas>
-      </div>
+        <div className={shapeClassName}>
+            <canvas id={id}></canvas>
+        </div>
     );
 }
 
-function determineCorners (anchorX, anchorY, length1, length2, orientation) {
-    if (orientation === 1) {
-        return  [[anchorX, anchorY],[anchorX + length2, anchorY],[anchorX, anchorY-length1]];
-    }
-    if (orientation === 2) {
-        return  [[anchorX, anchorY],[anchorX + length1, anchorY],[anchorX, anchorY+length2]];
-    }
-    if (orientation === 3) {
-        return  [[anchorX, anchorY],[anchorX, anchorY + length1],[anchorX-length2, anchorY]];
-    }
-    if (orientation === 4) {
-        return [[anchorX, anchorY],[anchorX - length1, anchorY],[anchorX, anchorY - length2]];
-    }
+function determineCorners(x, y, d1, d2, orientation) {
+    const orientations = {
+        1: [[x, y], [x + d1, y], [x, y - d2]],
+        2: [[x, y], [x + d1, y], [x, y + d2]],
+        3: [[x, y], [x - d1, y], [x, y + d2]],
+        4: [[x, y], [x - d1, y], [x, y - d2]],
+        5: [[x, y], [x + d2, y], [x, y + d1]],
+        6: [[x, y], [x - d2, y], [x, y - d1]]
+    };
+    return orientations[orientation]
 }
 
-
-
-export {Triangle, determineCorners}
+export { Triangle, determineCorners }
