433bed33b0aabbbdf98afcc98c9b411ef2e92344
Mohsen Aghajani <mohsen.agi@gmail.com>
2020-02-22 16:32:49 -0700

    refactored fence and added anchorDot to triangle

--- src/components/shapes/triangleComp.js
+++ src/components/shapes/triangleComp.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import "./shapeComp.css";
 
 function Triangle(props) {
-  // postion is array of anchorX, anchorY, length
   let {
     id,
     position,
@@ -10,23 +9,39 @@ function Triangle(props) {
     borderColour,
     borderWidth,
     shapeClassName
-  } = props.shapeInfo; //props.circleInfo -> props.shapeInfo
+  } = props.shapeInfo;
   let [anchorX, anchorY, size, orientation] = position;
+  let anchorDot = props.anchorDot;
   let length1 = size ;
   let length2 = size <=100 ? 100 : size/2;
+  
+  let cornerArray = determineCorners(
+    anchorX,
+    anchorY,
+    length1,
+    length2,
+    orientation
+  );
+
+  for (let corner of cornerArray) {
+    if (corner[1] < 0 || corner[1] > 2000) {
+        props.moveBack_shakeVertical();
+        break;
+    }
+  }
+
+  for (let corner of cornerArray) {
+    if (corner[0] < 0 || corner[0] > 2000) {
+        props.moveBack_shakeHorizontal();
+        break;
+    }
+  }
 
   useEffect(() => {
     var canvas = document.getElementById(id);
     var context = canvas.getContext("2d");
     canvas.width = 2000;
     canvas.height = 2000;
-    let cornerArray = determineCorners(
-      anchorX,
-      anchorY,
-      length1,
-      length2,
-      orientation
-    );
 
     function drawTriangle(fillColour, borderColour, borderWidth) {
       context.clearRect(0, 0, canvas.width, canvas.height);
@@ -37,13 +52,23 @@ function Triangle(props) {
       context.lineTo(cornerArray[0][0], cornerArray[0][1]);
       context.fillStyle = fillColour;
       context.fill();
+      context.lineWidth = borderWidth;
+      context.strokeStyle = borderColour;
+      context.stroke();
+    }
 
+    function drawAnchorDot(centerX, centerY, radius, fillColour, borderColour, borderWidth) {
+      context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = fillColour;
+      context.fill();
       context.lineWidth = borderWidth;
       context.strokeStyle = borderColour;
       context.stroke();
     }
 
     drawTriangle(fillColour, borderColour, borderWidth);
+    if (anchorDot) drawAnchorDot(anchorX, anchorY, size/20, 'white', 'white', borderWidth);
   }, [anchorX, anchorY, size, orientation]);
 
   return (
