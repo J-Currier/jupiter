4ae72c85ac7cd618a903b77fc80d25a5db5e7049
larryjl <leelawrencej@gmail.com>
2020-03-02 14:37:03 -0700

    fix: remove unused functions

--- src/scripts/math.js
+++ src/scripts/math.js
@@ -12,16 +12,16 @@ const mathFunctions = {
         return [anchorX, anchorY]
     },
 
-    createGridObject: () => {
-        const grid ={};
-        for (let x = -10; x <= 10; x++) {
-            grid[x] = {};
-            for (let y = -10; y <= 10; y++) {
-                grid[x][y] = false;
-            }
-        }
-    return grid
-    },
+    // createGridObject: () => {
+    //     const grid ={};
+    //     for (let x = -10; x <= 10; x++) {
+    //         grid[x] = {};
+    //         for (let y = -10; y <= 10; y++) {
+    //             grid[x][y] = false;
+    //         }
+    //     }
+    // return grid
+    // },
 
     translate: (x, y, deltaX, deltaY) => {
         let endX = x + deltaX;
@@ -64,11 +64,11 @@ const mathFunctions = {
         return (newOrientation);
     },
 
-    transformGrid: (grid, x, y, callback, parameters) => {
-        let [endX, endY] = callback(x, y, ...parameters);
-        grid[endX][endY] = true;
-        grid[x][y] = false;
-    }
+    // transformGrid: (grid, x, y, callback, parameters) => {
+    //     let [endX, endY] = callback(x, y, ...parameters);
+    //     grid[endX][endY] = true;
+    //     grid[x][y] = false;
+    // }
 }
 
 
