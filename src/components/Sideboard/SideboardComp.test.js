4f50b963518b5334db7e65b39f7a2a231866b20c
larryjl <leelawrencej@gmail.com>
2020-03-04 00:21:52 -0700

    fix: split out sideboard tab tests

--- src/components/Sideboard/SideboardComp.test.js
+++ src/components/Sideboard/SideboardComp.test.js
@@ -26,38 +26,66 @@ describe("sideboard load", () => {
       render(<SideboardComp />, container);
     });
   });
+})
 
-  test("click button", () => {
-    // setup mock button function
-    const mockButtonFx = jest.fn(); // mock function
-    const mockFactorHandle = jest.fn(); // mock function
+describe("sideboard tab clicks", () => {
+  let container = null;
+  let translate, rotate, reflect, handleChange, translationFactor, pivotPointx, pivotPointy, rotationMag, lineOfReflection;
+  let translationBtn, reflectionBtn, rotationBtn;
+  beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+
     act(() => {
       render(
         <SideboardComp
-          buttonFunction={mockButtonFx}
-          factorHandle={mockFactorHandle}
-          moveFactor={1}
+        translate={translate}
+        rotate={rotate}
+        reflect={reflect}
+        handleChange={handleChange}
+        translationFactor={translationFactor}
+        pivotPointx={pivotPointx}
+        pivotPointy={pivotPointy}
+        rotationMag ={rotationMag}
+        lineOfReflection={lineOfReflection}
         />,
         container
       );
     });
-    // identify the buttons
-    const translationBtn = document.getElementById("translation-button");
-    const rotationBtn = document.getElementById("rotation-button");
-    const reflectionBtn = document.getElementById("reflection-button");
+    translationBtn = document.getElementById("translation-button");
+    rotationBtn = document.getElementById("rotation-button");
+    reflectionBtn = document.getElementById("reflection-button");
+  });
 
+  afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  test("click translation", () => {
     // click translation
     act(() => {
       translationBtn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
     });
     expect(pretty(container.innerHTML)).toMatchSnapshot();
+  });
+
+  test("click rotation", () => {
     // click rotation
     act(() => {
       rotationBtn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
     });
+    expect(pretty(container.innerHTML)).toMatchSnapshot();
+  });
+
+  test("click reflection", () => {
     // click reflection
     act(() => {
       reflectionBtn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
     });
+    expect(pretty(container.innerHTML)).toMatchSnapshot();
   });
 });
