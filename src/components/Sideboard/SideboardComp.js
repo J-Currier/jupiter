55c633ed3f7426d06960fc54bfaa6d2e70dfdc13
larryjl <leelawrencej@gmail.com>
2020-03-04 14:58:15 -0700

    fix: sideboard navigation images and styling

--- src/components/Sideboard/SideboardComp.js
+++ src/components/Sideboard/SideboardComp.js
@@ -3,16 +3,19 @@ import './Sideboard.css'
 import Translation from '../translation/translationComp'
 import Rotation from '../rotation/rotationComp'
 import Reflection from '../reflection/reflectionComp'
+import { ReactComponent as TranslationSvg } from "../../images/btnTrans.svg";
+import { ReactComponent as RotationSvg } from "../../images/btnRot.svg";
+import { ReactComponent as ReflectionSvg } from "../../images/btnReflect.svg";
 
 function Sideboard(props) {
     const [tab, setTab] = useState(null)
 
     function handleClick(e) {
-        if(e.target.id === "translation-button"){
+        if(e.currentTarget.id === "translation-button"){
             setTab("translation")
-        }else if(e.target.id === "reflection-button"){
+        }else if(e.currentTarget.id === "reflection-button"){
             setTab("reflection")
-        }else if(e.target.id === "rotation-button"){
+        }else if(e.currentTarget.id === "rotation-button"){
             setTab("rotation")
         }
     };
@@ -20,30 +23,45 @@ function Sideboard(props) {
     return (
         <div id="sideboard">
             <nav>
-            <button id="translation-button" className='tab-button' onClick = {handleClick}>Translation</button>
-            <button id="rotation-button" className='tab-button' onClick = {handleClick}>Rotation</button>
-            <button id="reflection-button" className='tab-button' onClick = {handleClick}>Reflection</button>
+                <button id="translation-button" className='tab-button' onClick = {handleClick}>
+                    <TranslationSvg 
+                        alt="Translation"
+                    />
+                </button>
+                <button id="rotation-button" className='tab-button' onClick = {handleClick}>
+                    <RotationSvg 
+                        alt="Rotation"
+                    />
+                </button>
+                <button id="reflection-button" className='tab-button' onClick = {handleClick}>
+                    <ReflectionSvg
+                        alt="Reflection"
+                    />
+                </button>
             </nav>
             {tab === 'translation' &&  
                 <Translation 
-                translate = {props.translate}
-                translationFactor = {props.translationFactor}
-                handleChange = {props.handleChange}
-                />}
+                    translate = {props.translate}
+                    translationFactor = {props.translationFactor}
+                    handleChange = {props.handleChange}
+                />
+            }
             {tab === 'rotation' &&  
                 <Rotation 
-                rotate={props.rotate}
-                pivotPointx={props.pivotPointx}
-                pivotPointy={props.pivotPointy}
-                rotationMag ={props.rotationMag}
-                handleChange = {props.handleChange}
-                />}
+                    rotate={props.rotate}
+                    pivotPointx={props.pivotPointx}
+                    pivotPointy={props.pivotPointy}
+                    rotationMag ={props.rotationMag}
+                    handleChange = {props.handleChange}
+                />
+            }
             {tab === 'reflection' &&  
                 <Reflection 
-                reflect={props.reflect}
-                lineOfReflection={props.lineOfReflection}
-                handleChange = {props.handleChange}
-                />}
+                    reflect={props.reflect}
+                    lineOfReflection={props.lineOfReflection}
+                    handleChange = {props.handleChange}
+                />
+            }
         </div>
     )
 }
