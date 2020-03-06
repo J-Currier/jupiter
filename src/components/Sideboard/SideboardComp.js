bd1d95510d024ad76693b9bec242940d544723bf
larryjl <leelawrencej@gmail.com>
2020-03-06 11:12:10 -0700

    feat: sideboard nav svgs

--- src/components/Sideboard/SideboardComp.js
+++ src/components/Sideboard/SideboardComp.js
@@ -1,54 +1,78 @@
-import React, { useState } from 'react';
-import './Sideboard.css'
-import Translation from '../translation/translationComp'
-import Rotation from '../rotation/rotationComp'
-import Reflection from '../reflection/reflectionComp'
+import React, { useState } from "react";
+import "./Sideboard.css";
+import Translation from "../translation/translationComp";
+import Rotation from "../rotation/rotationComp";
+import Reflection from "../reflection/reflectionComp";
+import { ReactComponent as TranslationSvg } from "../../images/btnTrans.svg";
+import { ReactComponent as RotationSvg } from "../../images/btnRot.svg";
+import { ReactComponent as ReflectionSvg } from "../../images/btnReflect.svg";
 
 function Sideboard(props) {
-    const [tab, setTab] = useState(null)
+  const [tab, setTab] = useState(null);
 
-    function handleClick(e) {
-        if(e.target.id === "translation-button"){
-            setTab("translation")
-        }else if(e.target.id === "reflection-button"){
-            setTab("reflection")
-        }else if(e.target.id === "rotation-button"){
-            setTab("rotation")
-        }
-    };
+  function handleClick(e) {
+    if (e.currentTarget.id === "translation-button") {
+      setTab("translation");
+    } else if (e.currentTarget.id === "reflection-button") {
+      setTab("reflection");
+    } else if (e.currentTarget.id === "rotation-button") {
+      setTab("rotation");
+    }
+  }
 
-    return (
-        <div id="sideboard">
-            <nav>
-            <button id="translation-button" className='tab-button' onClick = {handleClick}>Translation</button>
-            <button id="rotation-button" className='tab-button' onClick = {handleClick}>Rotation</button>
-            <button id="reflection-button" className='tab-button' onClick = {handleClick}>Reflection</button>
-            </nav>
-            {tab === 'translation' &&  
-                <Translation 
-                translate = {props.translate}
-                translationFactor = {props.translationFactor}
-                handleChange = {props.handleChange}
-                addToStack={props.addToStack}
-                />}
-            {tab === 'rotation' &&  
-                <Rotation 
-                rotate={props.rotate}
-                pivotPointx={props.pivotPointx}
-                pivotPointy={props.pivotPointy}
-                rotationMag ={props.rotationMag}
-                handleChange = {props.handleChange}
-                addToStack={props.addToStack}
-                />}
-            {tab === 'reflection' &&  
-                <Reflection 
-                reflect={props.reflect}
-                lineOfReflection={props.lineOfReflection}
-                handleChange = {props.handleChange}
-                addToStack={props.addToStack}
-                />}
-        </div>
-    )
+  return (
+    <div id="sideboard">
+      <nav>
+        <button
+          id="translation-button"
+          className="tab-button"
+          onClick={handleClick}
+        >
+          <TranslationSvg alt="Translation" />
+        </button>
+        <button
+          id="rotation-button"
+          className="tab-button"
+          onClick={handleClick}
+        >
+          <RotationSvg alt="Rotation" />
+        </button>
+        <button
+          id="reflection-button"
+          className="tab-button"
+          onClick={handleClick}
+        >
+          <ReflectionSvg alt="Reflection" />
+        </button>
+      </nav>
+      {tab === "translation" && (
+        <Translation
+          translate={props.translate}
+          translationFactor={props.translationFactor}
+          handleChange={props.handleChange}
+          addToStack={props.addToStack}
+        />
+      )}
+      {tab === "rotation" && (
+        <Rotation
+          rotate={props.rotate}
+          pivotPointx={props.pivotPointx}
+          pivotPointy={props.pivotPointy}
+          rotationMag={props.rotationMag}
+          handleChange={props.handleChange}
+          addToStack={props.addToStack}
+        />
+      )}
+      {tab === "reflection" && (
+        <Reflection
+          reflect={props.reflect}
+          lineOfReflection={props.lineOfReflection}
+          handleChange={props.handleChange}
+          addToStack={props.addToStack}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Sideboard;
