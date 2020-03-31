c4febd9a4f944fe5c57c9308f76f5d27af5b8ad1
Mohsen Aghajani <mohsen.a@live.com>
2020-03-31 09:18:48 -0600

    Refactored Models

--- Graph Hopper API/Graph Hopper/Models/FunctionsRun.cs
+++ Graph Hopper API/Graph Hopper/Models/FunctionsRun.cs
@@ -6,11 +6,16 @@ namespace Graph_Hopper.Models
     {
         public long Id { get; set; }
         public long AttemptId { get; set; }
-        public DateTime RanAt { get; set; } = DateTime.Now; // Default
+        public DateTime RanAt { get; set; }
         public string Functions { get; set; }
         public string PlayerPositions { get; set; }
         public string PlayerAcceptablePositions { get; set; }
         public bool Success { get; set; }
         public int Score { get; set; }
+
+        public FunctionsRun()
+        {
+            RanAt = DateTime.Now;
+        }
     }
 }
