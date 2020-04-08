9995b16de4cc6ed45f4a7e9b80043574ce14c8f4
larryjl <leelawrencej@gmail.com>
2020-04-07 19:12:39 -0600

    fix: formatting

--- src/scripts/loadScript.js
+++ src/scripts/loadScript.js
@@ -1,20 +1,22 @@
-export default function loadScript (scriptId, url, callback) {
+export default function loadScript(scriptId, url, callback) {
   const existingScript = document.getElementById(scriptId);
   let script;
 
   if (!existingScript) {
-    script = document.createElement('script');
-    script.type = 'text/javascript';
+    script = document.createElement("script");
+    script.type = "text/javascript";
     script.src = url;
     script.id = scriptId;
     script.async = true;
     document.body.appendChild(script);
 
     script.onload = () => {
-      if (callback) callback();
+      if (callback) {
+        callback();
+      }
     };
   }
 
   if (existingScript && callback) callback();
   return script;
-};
+}
